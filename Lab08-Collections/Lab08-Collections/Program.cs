bd6353639cc026d436e610c0fc47b801cd740318
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-07-15 14:49:55 -0700

    added library collection

--- Lab08-Collections/Lab08-Collections/Program.cs
+++ Lab08-Collections/Lab08-Collections/Program.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Lab08_Collections.classes;
+using System;
+using System.Collections.Generic;
 
 namespace Lab08_Collections
 {
     class Program
     {
+        public Library<Book> Library { get; set; }
+        public List<Book> BookBag { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ Lab08-Collections/Lab08-Collections/classes/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08_Collections.classes
+{
+    class Library<T> : IEnumerable<T>
+    {
+        T[] items = new T[10];
+        int count;
+
+        /// <summary>
+        /// Adds an item to the generic collection
+        /// </summary>
+        /// <param name="item">Item being added</param>
+        public void Add(T item)
+        {
+            if (count == items.Length)
+            {
+                Array.Resize(ref items, items.Length + 5);
+            }
+            items[count++] = item;
+        }
+
+        /// <summary>
+        /// Returns the count of all the items in a collection
+        /// </summary>
+        /// <returns></returns>
+        public int Count()
+        {
+            return count;
+        }
+
+        /// <summary>
+        /// The method that allows a foreach to work on this custom generic collection
+        /// </summary>
+        /// <returns>Yield returns each item in the collection</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        /// <summary>
+        /// Supposedly magic leftover from an older time
+        /// </summary>
+        /// <returns>The enumerator</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
